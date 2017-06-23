2994d541953df795eab1c44271bc68e6e1da16b7
Aaron Eugene Kester <aaron.opta@yahoo.com>
2017-06-23 10:23:32 -0700

    went through and did lodash over the break

--- lodash.js
+++ lodash.js
@@ -6,64 +6,113 @@ var _ = {};
 
  // Returns the first element of an array.
 _.first = function(array) {
-  // Place your solution here	
+  if(array.length > 0) {
+    return array[0]
+  } else {
+    return null
+  }
 };
 
  // Returns the first n number of elements in an array.
 _.take = function(array, n) {
-	// Place your solution here	
+	let newArray = []
+  for (var i = 0; i < n; i++) {
+    newArray.push(array[i])
+  }
+  return (newArray)
 };
 
 // Returns the last element of an array.
-_.last = function(array) {  
-	// Place your solution here	
+_.last = function(array) {
+  return array[array.length - 1]
 };
 
 // Returns the last n number of elements in an array.
 _.takeRight = function(array, n) {
-	// Place your solution here	
+  let newArray = []
+  for (var i = array.length -n; i < array.length; i++) {
+    newArray.push(array[i])
+  }
+  return newArray
+
 };
 
-// Returns a new array with all falsey values removed. 
+// Returns a new array with all falsey values removed.
 // falsy values: false, null, 0, "", undefined, and NaN.
 // Example:
 // _.compact([0, 1, false, 2, '', 3]);
 // → [1, 2, 3]
 _.compact = function(array) {
-	// Place your solution here	
+  let newArray = []
+  for (var i = 0; i < array.length; i++) {
+    if (array[i]) {
+      newArray.push(array[i])
+    }
+  }
+  return  newArray
 };
 
-// Returns a new array of elements in the first argument, but 
+// Returns a new array of elements in the first argument, but
 // excludes any element found in the second argument.
 // Example:
 // _.difference([1, 2, 3], [4, 2]);
 // → [1, 3]
 _.difference = function(arrayOne, arrayTwo) {
-	// Place your solution here	
+  let newArray = []
+  let hold = false
+  for (var i = 0; i < arrayOne.length; i++) {
+    for (var j = 0; j < arrayTwo.length; j++) {
+      if (arrayOne[i] === arrayTwo[j]) {
+        hold = true;
+      }
+    }
+    if (!hold) {
+      newArray.push(arrayOne[i])
+    }
+    hold = false;
+  }
+  return newArray
 };
 
 // Returns element with minimum
 // value in an array.
 _.min = function(array) {
-	// Place your solution here	
+  return (Math.min.apply(null, array))
 };
 
 // Returns element with maximum
 // value in an array.
 _.max = function(array) {
-	// Place your solution here	
+  for (let x of array) {
+    if (max < x) {
+      max = item
+    }
+  }
+  return max
 };
 
 // Returns either index of matched element or
 // -1.
 _.indexOf = function(array, el) {
-	// Place your solution here	
+  for (var i = 0; i < array.length; i++) {
+    if (array[i] === el) {
+      return i
+    }
+  }
+  return -1
 };
 
 /*************** BONUS ***************/
 // Retuns a new array with elements in shuffled order.
 _.shuffle = function(array) {
-	// Place your solution here	
+  let newArray = []
+  let count = array.length
+
+  while (count > 0) {
+    newArray.push(array.splice(Math.random() * array.length),1) [0])
+    count--;
+  }
+  return newArray
 };
 
 /**************************************
@@ -71,35 +120,59 @@ _.shuffle = function(array) {
 **************************************/
 // Returns the length of a collection.
 _.size = function(collection) {
-	// Place your solution here	
+  if (typeof collection === 'object') {
+    return Object.keys(collection).length;
+  } else {
+    return collection.length
+  }
+  return collection.length
 };
 
-// Iterates on each element of a collection and 
+// Iterates on each element of a collection and
 // then returns the original collection.
 _.forEach = function(collection, callback) {
-	// Place your solution here	
-};
+  for (var x in collection) {
+    callback(collection[x])
+    }
+    return collection
+  }
 
-// Iterates on each element of a collection and 
+// Iterates on each element of a collection and
 // then returns a new array.
 _.map = function(collection, callback) {
-	// Place your solution here	
+  let newArray = []
+  for (var x in collection) {
+    newArray.push(callback(collection[x]))
+  }
+  return newArray
 };
 
 // Returns a new collection with filtered elements.
 _.filter = function(collection, callback) {
-	// Place your solution here	
+  let newArray = []
+  for (var x in collection) {
+    if (callback(collection)[x]) {
+      newArray.push(collection[x])
+    }
+  }
+  return newArray
 };
 
 // Returns a new collection with unfiltered elements.
 _.reject = function(collection, callback) {
-	// Place your solution here	
+  let newArray = []
+  for (var x in collection) {
+    if (!callback(collection[x])) {
+      newArray.push(collection[x])
+    }
+  }
+  return newArray
 };
 
 /*************** BONUS ***************/
  // Returns n number of elements in a collection.
 _.sample = function(collection, n) {
-	// Place your solution here	
+	// Place your solution here
 };
 
 module.exports = _;
